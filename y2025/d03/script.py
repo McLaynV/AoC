6bc818b14c259bcbe16faefcf84aaf45895524bf
McLayn <mclayn.v@gmail.com>
2025-12-03 21:57:24 +0100

    2025 03 part 2

--- y2025/d03/script.py
+++ y2025/d03/script.py
@@ -10,24 +10,31 @@ def produce_banks(file_name: str) -> Generator[str, None, None]:
             yield line.strip()
 
 
-def get_joltage_of_bank(bank: str) -> int:
-    first = max(bank[:-1])
-    first_index = indexOf(bank, first)
-    second = max(bank[first_index + 1:])
-    return int(first + second)
+def get_joltage_of_bank(bank: str, count: int) -> str:
+    if count == 1:
+        return max(bank)
+    strongest = max(bank[:-count + 1])
+    strongest_index = indexOf(bank, strongest)
+    remaining_bank = bank[strongest_index + 1:]
+    remaining_count = count - 1
+    return strongest + get_joltage_of_bank(remaining_bank, remaining_count)
 
 
 @validate_result
-def part1(file_name: str) -> int:
+def part(count: int, file_name: str) -> int:
     result = 0
     for bank in produce_banks(file_name):
-        result += get_joltage_of_bank(bank)
+        result += int(get_joltage_of_bank(bank, count))
     return result
 
 
 def main():
-    part1("example.txt", expected_result=357)
-    part1("input.txt", expected_result=17412)
+    part(2, "example.txt", expected_result=357)
+    part(2, "input.txt", expected_result=17412)
+    part(12, "example.txt", expected_result=3121910778619)
+    part(12, "input.txt", expected_result=172681562473501)
+
+    part(5, "multi_zero.txt", expected_result=98000)
 
 
 if __name__ == "__main__":
