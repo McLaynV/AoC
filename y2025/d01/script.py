45b9fd28b870519c32830fbda2078c4049426df2
McLayn <mclayn.v@gmail.com>
2025-12-03 21:19:11 +0100

    2025 01 part 2

--- y2025/d01/script.py
+++ y2025/d01/script.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Generator
+from typing import Generator, Literal
 
 from expectations_check import validate_result
+
+
+@dataclass
+class RotationSplit:
+    full_circles: int
+    partial: int
 
 
 class Rotation(int):
@@ -19,20 +25,42 @@ class Rotation(int):
                 raise ValueError(f"Unexpected direction '{value[0]}'.")
         return cls(direction * amount)
 
+    @property
+    def direction_sign(self) -> Literal[-1, 1]:
+        return -1 if self < 0 else 1
+
+    def split(self, circle_size: int) -> RotationSplit:
+        quotient, remainder = divmod(abs(self), circle_size)
+        return RotationSplit(full_circles=quotient, partial=remainder * self.direction_sign)
+
 
 @dataclass
 class Dial:
     size: int
     current_value: int
     landed_on_zero: int = 0
+    went_through_zero: int = 0
 
     def rotate(self, rotation: Rotation) -> None:
-        self.current_value += rotation
-        self.current_value %= self.size
-        if self.current_value < 0:
-            self.current_value += self.size
+        self._process_rotation(rotation)
         if self.current_value == 0:
             self.landed_on_zero += 1
+
+    def _process_rotation(self, rotation: Rotation) -> None:
+        rotation_split = rotation.split(self.size)
+        self.went_through_zero += rotation_split.full_circles
+        if not rotation_split.partial:
+            return
+        started_on_zero = (self.current_value == 0)
+        self.current_value += rotation_split.partial
+        if 0 < self.current_value < self.size:
+            return
+        if not started_on_zero:
+            self.went_through_zero += 1
+        if self.current_value < 0:
+            self.current_value += self.size
+        if self.current_value >= self.size:
+            self.current_value -= self.size
 
 
 def produce_rotations(file_name: str) -> Generator[Rotation, None, None]:
@@ -42,16 +70,18 @@ def produce_rotations(file_name: str) -> Generator[Rotation, None, None]:
 
 
 @validate_result
-def part1(file_name: str) -> int:
+def part(part2: bool, file_name: str) -> int:
     dial = Dial(size=100, current_value=50)
     for rotation in produce_rotations(file_name):
         dial.rotate(rotation)
-    return dial.landed_on_zero
+    return dial.went_through_zero if part2 else dial.landed_on_zero
 
 
 def main():
-    part1("example.txt", expected_result=3)
-    part1("input.txt", expected_result=1043)
+    part(False, "example.txt", expected_result=3)
+    part(False, "input.txt", expected_result=1043)
+    part(True, "example.txt", expected_result=6)
+    part(True, "input.txt", expected_result=5963)
 
 
 if __name__ == "__main__":
